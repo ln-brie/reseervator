d12f4869ec56539fb0f74965c1bdb836c073c78d
Helene <helenebrie@yahoo.fr>
2023-01-11 18:48:07 +0100

    dashboard ok

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,18 +2,24 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Reservation;
+use App\Entity\Room;
+use App\Entity\User;
+use App\Entity\Calendar;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
+    #[IsGranted('ROLE_ADMIN')]
     public function index(): Response
-    {
-        return parent::index();
+    {        
+        // return parent::index();
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
@@ -29,7 +35,7 @@ class DashboardController extends AbstractDashboardController
         // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
         // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
         //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        return $this->render('admin/dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -41,6 +47,13 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fa-solid fa-users', User::class)
+        ->setController(UserCrudController::class);
+        yield MenuItem::linkToCrud('Salles', 'fa-solid fa-location-dot', Room::class)
+        ->setController(RoomCrudController::class);
+        yield MenuItem::linkToCrud('Réservations', 'fa-solid fa-bookmark', Reservation::class)
+        ->setController(ReservationCrudController::class);
+        yield MenuItem::linkToCrud('Calendriers', 'fa-solid fa-calendar-days', Calendar::class)
+        ->setController(CalendarCrudController::class);
     }
 }
